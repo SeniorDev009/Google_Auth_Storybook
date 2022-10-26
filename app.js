3d7af9b12fddd854c4523fb78da5818d72791757
SeniorDev009 <greenforest19924@gmail.com>
2022-10-26 01:21:47 -0700

    create: DBconnect, configration

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const connectDB = require('./config/db');
+
+// Load config
+dotenv.config({path: "./.env"});
+
+connectDB();
+
+
+
+// initialize the app
+const app =  express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
